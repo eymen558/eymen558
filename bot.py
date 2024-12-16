b73c26aeb1e3f20e8e626ffada94c38e70c436ab
eymen558 <meymen1969@gmail.com>
2024-12-16 20:12:55 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,24 @@
+import discord
+
+intents = discord.Intents.default()
+
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'{client.user} olarak giriş yaptık.')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith('$merhaba'):
+        await message.channel.send("Selam!")
+    elif message.content.startswith('$bye'):
+        await message.channel.send("\U0001f642")
+    else:
+        await message.channel.send(message.content)
+
+client.run("MTMxODIyNzY2NDkwMDg1Mzc2MA.GTeRmH.U9R7Y3KkdNnhJhWbdJ0Pv_cf-9bb9JHB9z6MJk")
