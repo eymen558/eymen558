349813e95d330efb8d864f03b08a4bb672c2ace6
eymen558 <meymen1969@gmail.com>
2024-12-16 20:16:33 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,29 +1,36 @@
 import discord
+from discord.ext import commands
 
 intents = discord.Intents.default()
-
 intents.message_content = True
 
-client = discord.Client(intents=intents)
-
-@client.event
-async def on_ready():
-    print(f'{client.user} olarak giriş yaptık.')
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content.startswith('$merhaba'):
-        await message.channel.send("Selam!")
-    elif message.content.startswith('$bye'):
-        await message.channel.send("\U0001f642")
-    else:
-        await message.channel.send(message.content)
-
-client.run("MTMxODIyNzY2NDkwMDg1Mzc2MA.GTeRmH.U9R7Y3KkdNnhJhWbdJ0Pv_cf-9bb9JHB9z6MJk")
-
+bot = commands.Bot(command_prefix='$', intents=intents)
 
 @bot.event
 async def on_ready():
     print(f'{bot.user} olarak giriş yaptık')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Merhaba! Ben {bot.user}, bir Discord sohbet botuyum!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def sayhello(ctx):
+    await ctx.send("Merhaba Dostum!")
+    
+@cool.command(name='bot')
+async def _bot(ctx):
+    """Is the bot cool?"""
+    await ctx.send('Yes, the bot is cool.')
+
+bot.run("MTMxODIyNzY2NDkwMDg1Mzc2MA.GTeRmH.U9R7Y3KkdNnhJhWbdJ0Pv_cf-9bb9JHB9z6MJk")
+
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+    print('------')
